960990c570e465db5e2a73bb6ab2496b56357648
Sangam Puri <sangampuree@gmail.com>
2025-12-25 14:38:16 +0545

    created routes for login, register and dashboard pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('index');
 });
+
+
+Route::get('/login',function()
+{
+    return view('login');
+});
+Route::post('/login',[UserController::class,'login'])->name('login.perform');
+Route::get('/dashboard',[UserController::class,'dashboard'])->name('dashboard');
+
+
+Route::get('/register',function()
+{
+    return view('register');
+});
+Route::post('/register',[UserController::class,'register'])->name('register.save');
